a8b4f1dfb6783f05a8dc0049b0279cc4a8a2706b
Mauro Giamberardino <mgiamberardino@gmail.com>
2015-01-21 16:47:55 -0300

    Roman to Arabic finished point.

--- Topic3/src/org/maugiam/tdd/test/ArabicRomanConverterTest.java
+++ Topic3/src/org/maugiam/tdd/test/ArabicRomanConverterTest.java
@@ -3,10 +3,8 @@
  */
 package org.maugiam.tdd.test;
 
-import static org.junit.Assert.fail;
 import junit.framework.TestCase;
 
-import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -25,4 +23,35 @@ public class ArabicRomanConverterTest extends TestCase{
 		assertEquals(new Integer(1),ArabicRomanConverter.toArabic("I"));
 	}
 	
+	@Test
+	public void testNumber_5_To_Roman(){
+		assertEquals("V",ArabicRomanConverter.toRoman(5));
+	}
+	
+	@Test
+	public void testNumber_V_To_Arabic(){
+		assertEquals(new Integer(5),ArabicRomanConverter.toArabic("V"));
+	}
+	
+	@Test
+	public void testTenUnitNumber_To_Roman(){
+		assertEquals("XIV",ArabicRomanConverter.toRoman(14));
+	}
+	
+
+	@Test
+	public void testTenUnitNumber_To_Arabic(){
+		assertEquals(new Integer(14),ArabicRomanConverter.toArabic("XIV"));
+	}
+	
+	@Test
+	public void testComplexNumber_To_Roman(){
+		assertEquals("MCMXLIX",ArabicRomanConverter.toRoman(1949));
+	}
+	
+
+	@Test
+	public void testComplexNumber_To_Arabic(){
+		assertEquals(new Integer(1949),ArabicRomanConverter.toArabic("MCMXLIX"));
+	}
 }
