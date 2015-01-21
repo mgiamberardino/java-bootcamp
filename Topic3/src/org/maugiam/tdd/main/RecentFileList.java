a6a0fe8432dfb5ee39c2b4e0b59b4bf720ba9c5e
Mauro Giamberardino <mgiamberardino@gmail.com>
2015-01-21 11:12:38 -0300

    Class creation test.

---
+++ Topic3/src/org/maugiam/tdd/main/RecentFileList.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.maugiam.tdd.main;
+
+/**
+ * @author Mauro J Giamberardino
+ *
+ */
+public class RecentFileList {
+
+	protected Integer limit;
+	
+	public RecentFileList(Integer limit) {
+		this.limit=limit;
+	}
+
+}
--- Topic3/src/org/maugiam/tdd/test/RecentFileListTest.java
+++ Topic3/src/org/maugiam/tdd/test/RecentFileListTest.java
@@ -3,27 +3,33 @@
  */
 package org.maugiam.tdd.test;
 
-import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.maugiam.tdd.main.RecentFileList;
 
 /**
  * @author Mauro J Giamberardino
  *
  */
-public class RecentFileListTest {
+public class RecentFileListTest extends TestCase{
 
+	RecentFileList recent;
+	
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		recent=new RecentFileList(15);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreateInstance() {
+		assertNotNull(recent);
 	}
 
+	
+	
 }
