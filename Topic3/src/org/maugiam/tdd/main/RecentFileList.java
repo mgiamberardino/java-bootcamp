4b3619eb624adbb1431858dfb0cdb620fe39b300
Mauro Giamberardino <mgiamberardino@gmail.com>
2015-01-21 11:50:24 -0300

    Point 3 finished.

--- Topic3/src/org/maugiam/tdd/main/RecentFileList.java
+++ Topic3/src/org/maugiam/tdd/main/RecentFileList.java
@@ -3,6 +3,10 @@
  */
 package org.maugiam.tdd.main;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author Mauro J Giamberardino
  *
@@ -10,9 +14,26 @@ package org.maugiam.tdd.main;
 public class RecentFileList {
 
 	protected Integer limit;
+	protected LinkedList<String> fileList;
 	
 	public RecentFileList(Integer limit) {
+		this.fileList = new LinkedList<String>();
 		this.limit=limit;
+	}
+
+	public List<String> getRecentList() {
+		return fileList;
+	}
+
+	public void addOpenedFile(String filePath) {
+		int index = fileList.indexOf(filePath);
+		if(index<0){
+			if(fileList.size()>=limit)
+				fileList.removeLast();
+			fileList.push(filePath);
+		} else {
+			fileList.push(fileList.remove(index));
+		}
 	}
 
 }
